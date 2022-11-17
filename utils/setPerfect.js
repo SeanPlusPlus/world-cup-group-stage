a5894095eb53ed56898f1d48e7c3e31fe887a79d
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-11-17 14:24:42 -0800

    main: stub out set perf

---
+++ utils/setPerfect.js
@@ -0,0 +1,10 @@
+export const setPerfect = (entry, countries, entries) => {
+  console.log(entry)
+  console.log(countries)
+  console.log(entries)
+
+  let updatedCountries = {}
+
+
+  return { updatedCountries } 
+}
