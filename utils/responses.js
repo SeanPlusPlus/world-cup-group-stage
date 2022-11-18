1b9143aa17f75be27747bb23fa951c38ddd78c3e
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-11-18 11:12:08 -0800

    main: group stage

--- utils/responses.js
+++ utils/responses.js
@@ -1,20 +1,36 @@
+import _countBy from 'lodash/countBy'
+
+const getTotals = (teams, key, entries) => {
+  return teams.map((t) => {
+    const total = entries.filter((e) => {
+      return e[key].name === t.name
+    }).length
+
+    return {
+      ...t,
+      total
+    }
+  })
+}
+
 export const getResponses = (groups, countries, entries) => {
   const responses = []
   groups.forEach((g) => {
     const teams = countries[g]
     const first = {
-      name: `Group_1_${g}`,
+      name: `Group_${g}_1`,
       title: `Group ${g}`,
       icon: '🥇',
-      teams,
+      teams: getTotals(teams, `Group_${g}_1`, entries)
     }
     const second = {
-      name: `Group_2_${g}`,
+      name: `Group_${g}_2`,
       title: `Group ${g}`,
       icon: '🥈',
-      teams,
+      teams: getTotals(teams, `Group_${g}_2`, entries)
     }
     responses.push(first, second)
   })
+ 
   return responses
 }
